e164558e7c25a8f801612edec5b247306e4e17f2
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-16 13:16:53 +0200

    BiwgCommit

---
+++ 00 NUMBERS/script4.js
@@ -0,0 +1,19 @@
+const tableContainer = document.querySelector('.table-container');
+const table = document.createElement('table');
+let tr = document.createElement('tr');
+let count = 0;
+for (let index = 2; index < 50; index+=2) {
+    if (count === 5) {
+        table.appendChild(tr);
+        tr = document.createElement('tr');
+        count = 0;
+    }
+    const td = document.createElement('td');
+    td.textContent = i;
+    tr.appendChild(td);
+    count++;
+}
+if (count > 0) {
+    table.appendChild(tr);
+}
+tableContainer.appendChild(table);
