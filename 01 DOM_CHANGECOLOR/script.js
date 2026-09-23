ee826827e0f7677839b6145db4806c75095b4706
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-23 14:56:39 +0200

    Commit

--- 01 DOM_CHANGECOLOR/script.js
+++ 01 DOM_CHANGECOLOR/script.js
@@ -1,5 +1,14 @@
 const button = document.getElementById('colorBtn');
 const back = document.getElementById('body');
-if (button) {
-    back.style.color = "red";
-}
+button.addEventListener( "click", function () {
+    console.log("Hello World");
+    const colorID = 0;
+    if (colorID = 0) {
+        document.body.style.backgroundColor = "red";
+        colorID = 1;
+    } elseif (colorID = 1) {
+//      "#f0f4f8"
+        document.body.style.backgroundColor = "green";
+        colorID = 0;
+    }
+});
