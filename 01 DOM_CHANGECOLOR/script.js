7dec294f50df3dfd3c0a2a470d0c7f5032e169bf
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-24 13:02:23 +0200

    Commit

--- 01 DOM_CHANGECOLOR/script.js
+++ 01 DOM_CHANGECOLOR/script.js
@@ -1,14 +1,21 @@
 const button = document.getElementById('colorBtn');
-const back = document.getElementById('body');
+// const back = document.getElementById('body');
+let colorID = 0;
 button.addEventListener( "click", function () {
     console.log("Hello World");
-    const colorID = 0;
-    if (colorID = 0) {
-        document.body.style.backgroundColor = "red";
-        colorID = 1;
-    } elseif (colorID = 1) {
+    
+    if (colorID === 0) {
+        document.body.style.backgroundColor = "lightblue";
+       colorID = 1;
+    } 
+    else if (colorID === 1) {
 //      "#f0f4f8"
-        document.body.style.backgroundColor = "green";
+        document.body.style.backgroundColor = "lightgreen";
         colorID = 0;
     }
+});
+const caja = document.getElementById('colorBtn');
+caja.addEventListener('Mouse is out', () => {
+    console.log("Hello World");
+    caja.style.backgroundColor = 'red';
 });
